a68dc7e411c0ea93525f0fd43ccc7b3f0170a93f
kerby kaska <kerbykaska@gmail.com>
2022-07-11 09:10:55 -0700

    changed docker container to ubuntu instead of archlinux

--- docker/docker_build_image.sh
+++ docker/docker_build_image.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-
-LATEST_VERSION=`git ls-remote https://github.com/rpp0/gr-lora.git | grep HEAD | cut -f 1`
-docker build -t rpp0/gr-lora --build-arg CACHEBUST=$LATEST_VERSION .
-#docker tag rpp0/gr-lora:latest rpp0/gr-lora:$LATEST_VERSION
+LATEST_VERSION=`git ls-remote https://github.com/kerbstomps/gr-lora.git | grep HEAD | cut -f 1`
+docker build -t kerbstomps/gr-lora --build-arg CACHEBUST=$LATEST_VERSION .
