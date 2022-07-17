0e6122af1378a408974c80c065a338c51aa10ec8
kerby kaska <kerbykaska@gmail.com>
2022-07-17 09:51:39 -0700

    reverted back to archlinux to speed up container build. Modified realtime script to 915MHz and to output data to file.

--- docker/docker_run_grlora.sh
+++ docker/docker_run_grlora.sh
@@ -5,5 +5,5 @@ xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $DOCKER_XAUTH nmerge -
 docker run -i -t --rm --privileged -e DISPLAY=$DISPLAY -e XAUTHORITY=$DOCKER_XAUTH \
     -v /tmp/.docker.xauth:/tmp/.docker.xauth:ro \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    -v /dev/bus/usb:/dev/bus/usb \
+    -v /dev/:/dev/ \
     --entrypoint /bin/bash kerbstomps/gr-lora:latest
